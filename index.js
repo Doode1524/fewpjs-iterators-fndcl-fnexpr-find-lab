223f65809cf07097f924411c4d95cce1f71d5b34
Doode1524 <Doode1524@gmail.com>
2021-02-10 20:35:23 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const testVar = {}
 
 function testFunc() {
-  return "hi"
+    return "hi"
+}
+
+function superbowlWin(dates) {
+    let wins = dates.find(date => { return date.result === "W" })
+    if (typeof wins != "undefined")
+        return wins.year
 }
